72e0a394f5380fd427dc4f9b8f925c45343ccab0
Kauã Gabriell <kauagabriell64@gmail.com>
2026-02-09 16:24:34 -0300

    Criando o Middleware para pegar o body

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import http from 'node:http';
 const port = 3000;
 
+import { jsonHandler } from './middlewares/jsonHandler.js';
+
 //Iniciando Servidor
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+  await jsonHandler(req, res);
   res.end('Hello Word');
 });
 
