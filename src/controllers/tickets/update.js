2fb0af10db7ece63283ccbc148049ceadbb4b442
Kauã Gabriell <kauagabriell64@gmail.com>
2026-02-19 14:50:30 -0300

    Atualizando Ticket

--- src/controllers/tickets/update.js
+++ src/controllers/tickets/update.js
@@ -1,6 +1,12 @@
 export function updateTicket({ req, res, database }) {
   const { id } = req.params;
   const { equipment, description } = req.body;
-  
+
+  database.update('tickets', id, {
+    equipment,
+    description,
+    updated_at: new Date(),
+  });
+
   return res.end();
 }
